74c614caf1055ae5379fd024dc31365206af0fbf
Edward <edward@edward-VirtualBox.(none)>
2014-08-08 02:41:37 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatRoom::Application.config.secret_key_base = '62026337908f93e01c493c1c6403efe08edf103ea40c958cec6695a73afaf2325283082003fbf163a1b87d9443c0c1b58bb81cd4e0569ce79fade08bcacc9bbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChatRoom::Application.config.session_store :cookie_store, key: '_ChatRoom_session'
